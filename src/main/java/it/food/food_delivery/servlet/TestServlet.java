c36e94b66b9860008dda93c49a0c5032657e1cc4
FazziTux <riccardofazzi3@gmail.com>
2024-07-31 18:42:05 +0200

    Updated jakarta dependencies

--- src/main/java/it/food/food_delivery/servlet/TestServlet.java
+++ src/main/java/it/food/food_delivery/servlet/TestServlet.java
@@ -1,12 +1,11 @@
 package it.food.food_delivery.servlet;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(name = "TestServlet", urlPatterns = { "/hello" })
 //@WebServlet("/hello")
